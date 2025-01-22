86882684937ad37d1b50b509d5845e2eca884caf
amruthavarshiniabbimalla@gmail.com <amruthavarshiniabbimalla@gmail.com>
2025-01-22 06:59:50 +0530

    Successful

---
+++ CreateProfile.js
@@ -0,0 +1,12 @@
+function createProfile(){
+    let {name,email}=Obj;
+    let res={name:name,email:email}
+    return res;
+}
+let obj={
+    name:"Prachi",
+    age:25,
+    email:"prachi@gmail.com",
+    address:"st 123 gandhinagar"
+}
+console.log(createProfile(obj))
